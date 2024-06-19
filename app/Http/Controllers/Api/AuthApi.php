a44de1d8b3053cf48f49f9c79a7373db5b0b8313
Feras atiyat <ferasatiyat@gmail.com>
2024-06-19 03:30:04 +0300

    /

--- app/Http/Controllers/Api/AuthApi.php
+++ app/Http/Controllers/Api/AuthApi.php
@@ -7,36 +7,30 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules\Password;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Validator;
 
 class AuthApi extends Controller
 {
-  public function login(Request $request)
-{
-    $request->validate([
-        'user_name' => 'required',
-        'password' => 'required',
-    ]);
+    public function login(Request $request)
+    {
+        $request->validate([
+            'user_name' => 'required',
+            'password' => 'required',
+        ]);
 
-    $user = User::where('user_name', $request->user_name)->first();
+        $user = User::where('user_name', $request->user_name)->first();
 
-    if (!$user || !Hash::check($request->password , $user->password)) {
-        return response()->json(['result' => false],404);
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['result' => false], 404);
+        }
+
+        return response()->json(['result' => true]);
     }
 
-    return response()->json(['result' => true]);
-}
 
- 
 
-   
 
-    public function register(Request $request) 
+
+    public function register(Request $request)
     {
 
         $request->validate([
@@ -53,7 +47,4 @@ class AuthApi extends Controller
 
         return response()->json(['message' => 'Registration successful']);
     }
-
-
-
 }
